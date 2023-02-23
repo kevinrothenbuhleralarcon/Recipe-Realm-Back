fa728f0165d0b238d3272a85ca530bc7d0004fd5
Kevin Rothenbühler-Alarcon <kevin.rothenbuhler@gmail.com>
2023-02-23 20:29:41 +0100

    Initial commit

---
+++ src/main/java/ch/kra/reciperealm/RecipeRealmApplication.java
@@ -0,0 +1,13 @@
+package ch.kra.reciperealm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeRealmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeRealmApplication.class, args);
+	}
+
+}
